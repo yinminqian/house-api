9aed47295c783cca93e7ade064fe8a135721a0ea
尹敏乾 <yinminqian@yinminqiandeiMac.local>
2018-03-02 21:36:39 +0800

    debug

--- app/Http/Middleware/BsessionHandler.php
+++ app/Http/Middleware/BsessionHandler.php
@@ -19,12 +19,12 @@ class BsessionHandler
         $session_name = 'BSESSIONTOKEN';
         $token = $request->get($session_name) ?: $request->header($session_name);
         if (!$token || !Bs::valid($token)) {
-            dd(1);
 //            如果用户上传的数据没有携带token||数据库没有找到用户上传的token;
             Bs::generate();
 //           生成一条token进储存进数据库
 //           这时meta已经有数据
 
+            dd(1);
             $token = Bs::get_token();
 //            取到新的token;
             header("Access-Control-Expose-Headers: $session_name");
